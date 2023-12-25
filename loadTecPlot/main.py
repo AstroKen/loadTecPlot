20a5a8fbd978ba6036d216ae24e045d0251cbe56
AstroKen <eaukotaro@icloud.com>
2023-12-25 13:26:01 +0900

    code changed

--- loadTecPlot/main.py
+++ loadTecPlot/main.py
@@ -5,39 +5,36 @@ import re
 class loadTecPlot:
     def __init__(self, filename):
         self.file = filename
-        self.data = np.empty((0), dtype="object")
         self.noI = np.empty((0), dtype="int")
         self.noJ = np.empty((0), dtype="int")
         self.zones = np.empty((0), dtype="object")
 
     def loadFile(self):
         ndata = 0
-        datas = np.empty(0, dtype="float")
-        f = open(self.file, 'r')
-        for line in f:
-            line = line.lstrip()
-            if line.startswith('title') or line.startswith('TITLE'):
-                title = re.findall('"(.*?)"', line)
-            elif line.startswith('variables') or line.startswith('VARIABLES'):
-                vars = re.findall('"(.*?)"', line)
-                vars = np.array(vars)
-                nvars = len(vars)
-            elif line.startswith('zone') or line.startswith('ZONE'):
-                self.zones = np.append(self.zones, re.findall('"(.*?)"', line))
-                self.noI = np.append(self.noI, int(re.findall('[0-9]+', line)[0]))
-                self.noJ = np.append(self.noJ, int(re.findall('[0-9]+', line)[1]))
-            elif not line:
-                continue
-            else:
-                nums = line.split(' ')
-                for num in nums:
-                    try:
-                        num = float(num)
-                    except ValueError:
-                        continue
-                    datas = np.append(datas, num)
-                ndata += 1
-        f.close()
+        with open(self.file, 'r') as f:
+            data = f.read().splitlines()
+        nTitle = [i for i, s in enumerate(data) if "TITLE" in s]
+        nVariable = [i for i, s in enumerate(data) if "VARIABLES" in s]
+        nZone = [i for i, s in enumerate(data) if "ZONE" in s]
+
+        ###    load TITLE    ###
+        title = re.findall('"(.*?)"', data[nTitle[0]])
+
+        ###    load VARIABLES    ###
+        vars = re.findall('"(.*?)"', data[nVariable[0]])
+        vars = np.array(vars)
+        nvars = len(vars)
+
+        ###    load ZONE    ###
+        self.zones = np.append(self.zones, re.findall('"(.*?)"', data[nZone[0]]))
+        self.noI = np.append(self.noI, int(re.findall('[0-9]+', data[nZone[0]])[0]))
+        self.noJ = np.append(self.noJ, int(re.findall('[0-9]+', data[nZone[0]])[1]))
+
+        ###    load data    ###
+        ### TODOs  extend the method for multiple ZONE data ###
+        vals = [i for i in data[nZone[0]+1:] if i != ' ']
+        val = map(lambda vallist: vallist.split(), vals)
+        datas = np.array(list(val), dtype='float')
 
         size = np.empty(self.zones.size + 1, dtype="int")
         size[0] = 0
@@ -47,6 +44,4 @@ class loadTecPlot:
         for i in range(self.zones.size):
             indicies[size[i]:size[i + 1]] = self.zones[i]
 
-        datas = datas.reshape(np.sum(self.noI*self.noJ), nvars)
         self.data = pd.DataFrame(datas, columns=vars, index=indicies)
-        print(self.data)
