f298b7ab4519205a1d8df8d1969d73830c8ef512
AstroKen <eaukotaro@icloud.com>
2024-02-11 11:32:07 +0900

    code changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="loadTecPlot",
-    version="0.0.1",
+    version="0.0.2",
     description='Package for load TecPlot format files.',
     author="AstroKen",
     packages=find_packages(),
