608e8ff2fb8e49af797a1adc1724115d32ab8d31
AstroKen <eaukotaro@icloud.com>
2024-02-12 01:05:44 +0900

    code changed

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="loadTecPlot",
-    version="0.0.2",
-    description='Package for load TecPlot format files.',
+    version="0.0.3",
+    description='Package for load TecPlot format files. SSH connection is added.',
     author="AstroKen",
     packages=find_packages(),
     install_requires=[
         "numpy",
-        "pandas"
+        "pandas",
+        "paramiko"
         ],
     include_package_data=True,
 )
